865c5550a266bf4a9e0a5b7f5d7e8a5ad0f70561
isophel <natisox18@gmail.com>
2021-11-02 23:42:18 -0700

    Editted settings.py

--- xploreBot/settings.py
+++ xploreBot/settings.py
@@ -168,6 +168,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_URL = os.environ.get('AWS_URL')
 AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 STATIC_URL = AWS_URL + '/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = AWS_URL + '/media/'
